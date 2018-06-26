d4da3d91b6bf1335ce79429e4bb325e55a438db3
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2018-06-26 22:34:27 +0200

    added dnsmasq to consul forward and set dnsmasq as first dns server

--- ami/setup_amazon-linux.sh
+++ ami/setup_amazon-linux.sh
@@ -10,3 +10,11 @@ echo "[INFO] [${SCRIPT}] Setup docker"
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+
+echo "[INFO] [${SCRIPT}] Setup dnsmasq"
+sudo yum install -y dnsmasq
+sudo chkconfig --level 345 dnsmasq on
+
+sudo mkdir -p /etc/dnsmasq.d
+echo "server=/consul/127.0.0.1#8600" | sudo tee /etc/dnsmasq.d/10-consul
+echo "prepend domain-name-servers 127.0.0.1;" | sudo tee -a /etc/dhcp/dhclient.conf
